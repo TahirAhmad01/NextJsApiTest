bda91ee6bc4162e0860721ec01b3a7698873aa59
Tahir Ahmad <tahirahmad0234@gmail.com>
2023-04-24 13:58:51 +0600

    next api added

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,15 @@ import { useEffect, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+export default function Home({ todo }) {
+  console.log(todo);
   const [userList, setUserList] = useState([]);
   const [userVal, setUser] = useState("");
   const LastUser = userList.slice(-1)[0];
 
   function getUser() {
+    // console.log(data);
+
     axios.get("/api/user").then((response) => {
       // console.log(response.data);
       setUserList(response.data);
@@ -59,4 +62,32 @@ export default function Home() {
       </div>
     </div>
   );
+}
+
+export const checkEnvironment = () => {
+  let base_url =
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3001"
+      : "https://example.com"; // https://v2ds.netlify.app
+
+  return base_url;
+};
+
+export async function getServerSideProps(ctx) {
+  const res = await fetch(
+    process.env.NODE_ENV === "development"
+      ? `http://${ctx.req?.headers.host}/api/user`
+      : `https://${ctx.req?.headers.host}/api/user`
+  );
+  const data = await res.json();
+
+  if (!data) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: { todo: data }, // will be passed to the page component as props
+  };
 }
